c1a847393757e2aec5f1b69921d3bff1e19cd38a
Angelo Pavilando <pavilandoangelo@gmail.com>
2024-03-06 23:31:50 +0800

    change pre commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Open_Sans } from 'next/font/google';
 import './globals.css';
 
-const openSans = Open_Sans({ subsets: ['latin'] })
+const openSans = Open_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
     title: 'Orchard Frontend Test',
